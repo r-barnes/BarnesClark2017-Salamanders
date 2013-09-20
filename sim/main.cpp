c2d56262a8fc6861978f173006a4a80847ce06b4
Richard Barnes <rbarnes@umn.edu>
2013-09-20 13:56:52 -0500

    Further work with setup code

--- sim/main.cpp
+++ sim/main.cpp
@@ -3,7 +3,11 @@
 using namespace std;
 
 int main(){
-  array< array<Salamander, 1000>, 10000> mts;
+  //One elevation bin of the mountain
+  typedef array<Salamander, 1000> mtbin;
+
+  //Profile of the mountain
+  array<mtbin, 10000> mts;
 
   //Start off by killing everything
   for(unsigned int x=0;x<mts.size();++x)
@@ -13,4 +17,10 @@ int main(){
   //Find cell corresponding to optimal happy temperature
   //Make a salamander in that cell alive
   //Go forth
+
+  //Salamanders are at their very happiest at 12.804279 degC
+  //At sea level 65MYA the temp was 33.5618604122814 degC
+  //Temperature drops at 9.8 degC/1000m (adiabatic lapse rate of dry air)
+  //So (33.5618604122814-12.804279)/9.8=2.1181*1000m=2.11km
+  
 }
