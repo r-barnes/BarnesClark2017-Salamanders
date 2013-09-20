f72f321be1c814b6b2049ddd6b5193b0bff8febe
Richard Barnes <rbarnes@umn.edu>
2013-09-20 13:13:31 -0500

    Added missing files

---
+++ sim/data.hpp
@@ -0,0 +1,5 @@
+#ifndef _data
+#define _data
+double temps;
+
+#endif
---
+++ sim/utility.hpp
@@ -0,0 +1,14 @@
+#ifndef _utility
+#define _utility
+
+double gaussrand();
+
+template<class T>
+T countbits(T a){
+  unsigned int c; // c accumulates the total bits set in combined
+  for (c = 0; a; ++c)
+    a &= a - 1; // clear the least significant bit set
+  return c;
+}
+
+#endif
