fb70ab8a527c5502c09a503006c41bd106af17a6
Richard Barnes <rbarnes@umn.edu>
2013-09-20 15:32:28 -0500

    Added height(), started Mortaliate(), added killAll(), added addSalamander()

--- sim/mtbin.cpp
+++ sim/mtbin.cpp
@@ -1,6 +1,9 @@
 #include "mtbin.hpp"
+#include "data.hpp"
 
-MtBin::MtBin(){}
+MtBin::MtBin(){
+  startofdead=0;
+}
 
 MtBin::MtBin(double binx) : MtBin(binx) {}
 
@@ -8,10 +11,28 @@ void MtBin::Mortaliate() {
   double mytemp=temp();
 
   for(auto &s: bin){
-    if(s.pDie(mytemp)){}
+    if(s.pDie(mytemp)){
+      s.dead=true;
+    }
   }
 }
 
 double MtBin::temp() const {
   
+}
+
+double MtBin::height() const {
+
+}
+
+void MtBin::killAll() {
+  for(auto &s: bin)
+    s.dead=true;
+  startofdead=0;
+}
+
+void MtBin::addSalamander(const Salamander &s) {
+  if(startofdead==bin.size()) return;
+  bin[startofdead]=s;
+  ++startofdead;
 }
--- sim/mtbin.hpp
+++ sim/mtbin.hpp
@@ -11,11 +11,26 @@ class MtBin {
 
     std::array<Salamander, 1000> bin;
     double binx;
+
+    ///Location of the first dead salamander on the list
+    ///if startofdead==bin.size() then there are no more
+    ///openings in the population
+    int startofdead;
+
     ///Kill random salamanders with probability prob
     void mortaliate();
 
-    ///Return the temperature of the bin
-    double temp() const;
+    ///Return the temperature of the bin at a given time
+    double temp(double t) const;
+
+    ///Return the height of the bin at a given time
+    double height(double t) const;
+
+    ///Kill all the salamanders in the bin and reset startofdead
+    void killAll();
+
+    ///Add a salamander to the bin. Fail silently if there's no room.
+    void addSalamander(const Salamander &s);
 };
 
 #endif
