406fb7d7f4b8c0b86d92fdec49440443a85fe19b
Richard Barnes <rbarnes@umn.edu>
2013-09-20 15:58:12 -0500

    Specified constants

--- sim/utility.cpp
+++ sim/utility.cpp
@@ -3,11 +3,11 @@
 
 ///Give elevation as altitude in kilometers, and t in kiloyears ago (i.e. 0.001MYA)
 double MountainElevation(double elevation, double time) {
-  double elek=12.029753;
-  double elesigma=0.211410;
-  double elemu=0.245547;
-  double pi=3.141593;
-  double deltasd=2.881572e-09;
+  const double elek=12.029753;
+  const double elesigma=0.211410;
+  const double elemu=0.245547;
+  const double pi=3.141593;
+  const double deltasd=2.881572e-09;
 
   double area = elek
                 * ( 1/sqrt( 2*pi*pow(elesigma+time*deltasd*1000, 2) ))
