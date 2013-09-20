61ee275d9baa35db46ac661d98c8c26d8ae17c02
Richard Barnes <rbarnes@umn.edu>
2013-09-20 13:38:34 -0500

    Outlined initialization procedure

--- sim/main.cpp
+++ sim/main.cpp
@@ -3,5 +3,14 @@
 using namespace std;
 
 int main(){
-  array< array<Salamander, 1000>, 10000> pop;
+  array< array<Salamander, 1000>, 10000> mts;
+
+  //Start off by killing everything
+  for(unsigned int x=0;x<mts.size();++x)
+    for(auto &s: mts[x])
+      s.dead=true;
+
+  //Find cell corresponding to optimal happy temperature
+  //Make a salamander in that cell alive
+  //Go forth
 }
