61b956cb901c71319d01659963f3d5dc30fad08e
Richard Barnes <rbarnes@umn.edu>
2013-09-20 19:44:45 -0500

    Fixed compilation errors by including phylo.o

--- sim/phylo.cpp
+++ sim/phylo.cpp
@@ -2,7 +2,7 @@
 #include "salamander.hpp"
 #include <cstdlib>
 
-Phylo::Phylo(const Salamander &s, double t);
+Phylo::Phylo(const Salamander &s, double t){
   genes=s.genes;
   emergence=t;
   parent=s.parent;
--- sim/phylo.hpp
+++ sim/phylo.hpp
@@ -6,7 +6,7 @@
 class Phylo {
   public:
     ///Innitialize new salamandar
-    Phylo(Salamander &s, double t);
+    Phylo(const Salamander &s, double t);
 
     ///Genome of the strain
     Salamander::genetype genes;
