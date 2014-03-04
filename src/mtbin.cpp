a6a365e2003d10e984d17ba81d93b7bdedf0e100
Richard Barnes <rbarnes@umn.edu>
2014-03-03 16:03:53 -0800

    Aligned equals sign

--- src/mtbin.cpp
+++ src/mtbin.cpp
@@ -9,11 +9,11 @@
 
 ///Give elevation as altitude in kilometers, and t in kiloyears ago (i.e. 0.001MYA)
 double MountainArea(double elevation, double time) {
-  const double elek=12.029753;
-  const double elesigma=0.211410;
-  const double elemu=0.245547;
-  const double pi=3.141593;
-  const double deltasd=2.881572e-09;
+  const double elek     = 12.029753;
+  const double elesigma = 0.211410;
+  const double elemu    = 0.245547;
+  const double pi       = 3.141593;
+  const double deltasd  = 2.881572e-09;
 
   double area = elek
                 * ( 1/sqrt( 2*pi*pow(elesigma+time*deltasd*1000, 2) ))
