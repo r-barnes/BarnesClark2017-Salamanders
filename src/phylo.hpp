e28ff0bed5219ceaa845755415f424cb58d8edc8
adamtclark <adam.tclark@gmail.com>
2014-03-03 22:29:58 -0800

    Added comments to phylo

--- src/phylo.hpp
+++ src/phylo.hpp
@@ -8,21 +8,29 @@
 
 class PhyloNode {
   public:
-    ///Innitialize new salamandar
+    ///Innitialize new salamandar in the phylogeny. This is used to figure out
+    ///which species and lineage each salamander belongs to. 
     PhyloNode(const Salamander &s, double t);
-    ///Genome of the strain
+    ///Genome of the strain. Inherited from salamandar::Salamander().
     Salamander::genetype genes;
-    ///When this strain emerged
+    ///When this strain emerged. emergence describes the time that
+    ///the species arises, and is taken as the current t.
     double emergence;
-    ///Last time this strain had a child
+    ///Last time this strain had a child. lastchild is used to identify species
+    ///that have gone extinct, and is updated with the current time t every
+    ///time that a living salamader is identified a member of the
+    ///species recorded by PhyloNode.
     double lastchild;
-    ///Which strain this one emerged from
+    ///Which strain this one emerged from? Inherited from salamandar::Salamander().
     int parent;
-    ///Which otemp did the first parent inherit?
+    ///Which otemp did the first parent inherit? Inherited from salamandar::Salamander().
     double otemp;
-    ///Children of this node
+    ///Children of this node. Lists all species that have branched off of the
+    ///species described by PhyloNode.
     std::vector<int> children;
-    ///Adds a child to this node
+    ///Adds a child to this node. Child is represented by an integer, which lists
+    ///all species as sequential integers starting at 0, based on their time of
+    ///emergence.
     void addChild(int n);
 };
 
