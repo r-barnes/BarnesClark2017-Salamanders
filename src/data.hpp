42f27d7a7f448edbdbe60fe73bb621eeebd1bfa3
Richard Barnes <rbarnes@umn.edu>
2014-03-19 15:23:33 -0500

    Made temperature a singleton class rather than a global array

--- src/data.hpp
+++ src/data.hpp
@@ -1,5 +1,26 @@
-#ifndef _data
-#define _data
-extern double temps[];
+#ifndef _temp_hpp_
+#define _temp_hpp_
+
+#include <vector>
+
+//Define a singleton class for Temperatures
+class Temperature {
+ private:
+  std::vector<double> temps;
+  bool test;
+  double testTemp;
+  Temperature();                               //Prevent construction
+  Temperature(const Temperature&);             //Prevent copying
+  Temperature& operator=(const Temperature&);  //Prevent assignment
+ public:
+  static Temperature& getInstance() {
+    static Temperature instance;
+    return instance;
+  }
+  void init(const char filename[]);
+  double getTemp(double tMyrs) const;
+  void testOn(double temp);
+  void testOff();
+};
 
 #endif
--- src/test.cpp
+++ src/test.cpp
@@ -2,6 +2,7 @@
 #include "mtbin.hpp"
 #include "phylo.hpp"
 #include "simulation.hpp"
+#include "data.hpp"
 #include <array>
 #include <vector>
 #include <iostream>
@@ -11,6 +12,12 @@
 using namespace std;
 
 int main(){
+  Temperature::getInstance().init("../data/temp_series_degreesC_0_65MYA_by_0.001MY.csv");
+
+  cerr<<"Verifying temperatures"<<endl;
+  for(double tMyrs=0;tMyrs<65;tMyrs+=0.5)
+    cerr<<"Temperature at "<<tMyrs<<"\t=\t"<<Temperature::getInstance().getTemp(tMyrs)<<endl;
+
   MtBinUnitTest mtbintest;
   mtbintest.run();
 }
