1d3df5d23e0c284440e91e45b3405ce964843a62
Richard Barnes <rbarnes@umn.edu>
2016-08-07 00:14:35 -0600

    Switch to config file

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@
 #include <fstream>
 #include <cstdlib>
 #include <string>
+#include <stdexcept>
 using namespace std;
 
 std::string SimulationSummaryHeader() {
@@ -35,6 +36,46 @@ void printSimulationSummary(ofstream &out, int r, const Simulation &sim){
   out<<endl;
 }
 
+void Input_CheckParamName(std::ifstream &fparam, const std::string &param_name){
+  std::string in_param_name;
+  fparam>>in_param_name;
+  if(in_param_name!=param_name){
+    std::cerr<<"Unexpected parameter name! Found '"<<in_param_name<<"' expected '"<<param_name<<"'"<<endl;
+    throw std::runtime_error("Unexpected parameter name!");
+  }
+}
+
+std::string Input_Filename(std::ifstream &fparam, const std::string &param_name){
+  Input_CheckParamName(fparam, param_name);
+  std::string temp;
+  fparam>>temp;
+  return temp;
+}
+
+bool Input_YesNo(std::ifstream &fparam, const std::string &param_name){
+  Input_CheckParamName(fparam, param_name);
+  std::string temp;
+  fparam>>temp;
+  if(! (temp=="YES" || temp=="NO")){
+    cerr<<"Paremeter '"<<param_name<<"' must be YES or NO!"<<endl;
+    throw std::runtime_error("Bad parameter value!");
+  }
+  return (temp=="YES");
+}
+
+double Input_Double(std::ifstream &fparam, const std::string &param_name){
+  Input_CheckParamName(fparam, param_name);
+  double temp;
+  fparam>>temp;
+  return temp;
+}
+
+int Input_Integer(std::ifstream &fparam, const std::string &param_name){
+  Input_CheckParamName(fparam, param_name);
+  int temp;
+  fparam>>temp;
+  return temp;
+}
 
 int main(int argc, char **argv){
   bool vary_temp;
@@ -49,57 +90,66 @@ int main(int argc, char **argv){
   double tdrift;
   double simthresh;
 
-  //The simulation timestep. This choice is somewhat arbitrary.
-  const double timestep = 0.5; //My
+  std::string temp_series_filename = "data/temp_series_degreesC_0_65MYA_by_0.001MY.csv";
 
-  //NOTE: This line should be uncommented before running simulations for
-  //purposes other than testing.
-  seed_rand();
+  //The simulation timestep.
+  double timestep = 0.5; //My
 
-  if(argc!=12){
-    cout<<"Syntax: "<<argv[0]<<" <Summary Stats> <Persistence Graph Output Base> ";
-    cout<<"<Phylogeny Output Base> <SpeciesStats Output Base> <(No)VaryHeight> ";
-    cout<<"<(No)VaryTemp> <RunOnce/RunMany> <Maximum Iterations> <Mutation Prob (~1e-3)> ";
-    cout<<"<Temperature Drift Rate (~0.1)> <Species Similarity Threshold (~0.95)>"<<endl;
-    cout<<"'once' indicates the program should be run once, for testing."<<endl;
-    cout<<"Names marked base will have file extensions automatically appended."<<endl;
+  if(argc!=2){
+    cout<<"Syntax: "<<argv[0]<<" <Parameters File>\n";
+    cout<<"Parmeters file can contain:\n";
+    cout<<"\tPARAM                     TYPE        DESCRIPTION\n";
+    cout<<"\tSummaryStatsFilename      Filename               \n";
+    cout<<"\tPersistenceGraphFilename  Filename               \n";
+    cout<<"\tPhylogenyFilename         Filename               \n";
+    cout<<"\tSpeciesStatsFilename      Filename               \n";
+    cout<<"\tVaryHeight                YES/NO                 \n";
+    cout<<"\tVaryTemp                  YES/NO                 \n";
+    cout<<"\tRunOnce                   YES/NO                 \n";
+    cout<<"\tMutationProb              Double                 \n";
+    cout<<"\tTemperatureDrift          Double                 \n";
+    cout<<"\tSpeciesSimilarity         Double                 \n";
+    cout<<"\ttimestep                  Double      Units are in My.\n";
+    cout<<"\tmaxiter                   Integer     Numer of times to run each parameter combination.\n";
+    cout<<"\tPRNGseed                  Integer     Use 0 for true entropy; otherwise, seed engine to given value.\n";
+    cout<<"\tTempSeries                Filename    If not specified, a default time series is used. NO SPACES ALLOWED!\n";
     return -1;
-  } else {
-    out_summary              = argv[1];
-    out_persist              = argv[2];
-    out_phylogeny            = argv[3];
-    out_species_stats        = argv[4];
-    string vary_height_or_no = argv[5];
-    string vary_temp_or_no   = argv[6];
-    string run_once_or_many  = argv[7];
-    maxiter                  = stoi(std::string(argv[8]));
-    mprob                    = stod(std::string(argv[9]));
-    tdrift                   = stod(std::string(argv[10]));
-    simthresh                = stod(std::string(argv[11]));
-
-    if( !(vary_height_or_no=="NoVaryHeight" || vary_height_or_no=="VaryHeight")){
-      cerr<<"Unrecognised vary height directive."<<endl;
-      return -1;
-    }
-    if( !(vary_temp_or_no=="NoVaryTemp" || vary_temp_or_no=="VaryTemp")){
-      cerr<<"Unrecognised vary temp directive."<<endl;
-      return -1;
-    }
-    if( !(run_once_or_many=="RunOnce" || run_once_or_many=="RunMany")){
-      cerr<<"Unrecognised vary temp directive."<<endl;
-      return -1;
-    }
-    vary_height = (vary_height_or_no == "VaryHeight");
-    vary_temp   = (vary_temp_or_no   == "VaryTemp"  );
-    run_once    = (run_once_or_many  == "RunOnce"   );
   }
 
-  Temperature::getInstance().init("data/temp_series_degreesC_0_65MYA_by_0.001MY.csv");
+  std::ifstream fparam(argv[1]);
+  out_summary       = Input_Filename(fparam,"SummaryStatsFilename");
+  out_persist       = Input_Filename(fparam,"PersistenceGraphFilename");
+  out_phylogeny     = Input_Filename(fparam,"PhylogenyFilename");
+  out_species_stats = Input_Filename(fparam,"SpeciesStatsFilename");
+
+  vary_height = Input_YesNo(fparam,"VaryHeight");
+  vary_temp   = Input_YesNo(fparam,"VaryTemp");
+  run_once    = Input_YesNo(fparam,"RunOnce");
+
+  mprob     = Input_Double(fparam, "MutationProb");
+  tdrift    = Input_Double(fparam, "TemperatureDrift");
+  simthresh = Input_Double(fparam, "SpeciesSimilarity");
+  timestep  = Input_Double(fparam, "timestep");
+
+  maxiter = Input_Integer(fparam,"maxiter");
+  seed_rand(Input_Integer(fparam,"PRNGseed"));
+
+  temp_series_filename = Input_Filename(fparam,"TempSeries");
+
+  {
+    std::string test_if_file_is_empty;
+    if(fparam>>test_if_file_is_empty){
+      std::cerr<<"File contained unexpected parameters at the end!"<<std::endl;
+      return -1;
+    }
+  }
+
+  Temperature::getInstance().init(temp_series_filename);
   if(!vary_temp) {
     Temperature::getInstance().testOn(34); //km CHANGE ADDED TO TEST NO TEMP CHANGE
   }
 
-  if(argc==5 && run_once){
+  if(run_once){
     Simulation sim(0.001, 0.01, 0.96, 1, timestep, vary_height);
     sim.runSimulation();
 
